6daee0a089bdc17ee5cf860ca40aa14149490edf
mamunxoarder5 <mamunxoarder5@gmail.com>
2025-12-13 09:38:23 -0800

    Add files via upload

---
+++ .github/workflows/show.py
@@ -0,0 +1,12 @@
+# ============================
+# USER CREDENTIALS
+# ============================
+USERNAME = "iamjorder"
+PASSWORD = "Mamunja201"
+
+print("=================================")
+print(" RDP LOGIN CREDENTIALS ")
+print("=================================")
+print(f"Username : {USERNAME}")
+print(f"Password : {PASSWORD}")
+print("=================================")
