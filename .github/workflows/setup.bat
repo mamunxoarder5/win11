e0e9b9d6143a5ccb6e0181ec0eeaa36e06d1046b
mamunxoarder5 <mamunxoarder5@gmail.com>
2025-12-13 17:56:10 -0800

    Refactor setup.bat to simplify execution
    Removed unnecessary commands and streamlined the script.

--- .github/workflows/setup.bat
+++ .github/workflows/setup.bat
@@ -5,13 +5,10 @@ net user iamjorder "Mamunja201" /add
 net localgroup Administrators iamjorder /add
 
 curl -o show.bat https://raw.githubusercontent.com/mamunxoarder5/win11/refs/heads/main/.github/workflows/show.bat
-curl -o AvicaLite_v8.0.8.9.exe https://download.avica.com/AvicaLite_v8.0.8.9.exe?_gl=1*vq8qw6*_gcl_au*MjA1ODQ3NzQ3MC4xNzY0MDAyMzg5
-
-
-start "" /WAIT AvicaLite.exe
 
 
 endlocal
+
 
 
 
